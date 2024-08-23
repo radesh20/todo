6227288455b54d290bad4d0a110b20163e6255a6
Radesh Chakravarthy <131537022+radesh20@users.noreply.github.com>
2024-08-23 21:58:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,13 +6,13 @@ import { getAnalytics } from "firebase/analytics";
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyCDVH1zK_mOJrW8kyCwPYirpvoZXXRIfS8",
-  authDomain: "react-todo-56dc9.firebaseapp.com",
-  projectId: "react-todo-56dc9",
-  storageBucket: "react-todo-56dc9.appspot.com",
-  messagingSenderId: "909811265888",
-  appId: "1:909811265888:web:79da401e6b7cc31b3c2e05",
-  measurementId: "G-R6133RYGXB"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
